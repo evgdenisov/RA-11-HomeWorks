f9d37e8853e64e839d4cb0deca86bb024430b74d
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-06-03 10:51:17 +0300

    Add files via upload

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,17 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
-};
+
+class SearchBox extends React.Component {
+    
+    render() {
+        let props = this.props;
+            return (
+                <input 
+                type="text" 
+                placeholder="Поиск по названию или автору"
+                value={props.value}
+                onChange={event => props.filterBooks(event.currentTarget.value)}
+                />
+            );
+        
+        }
+    
+}
